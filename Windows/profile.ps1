af163c0a8575745c1e6ee2185f82a8da7bc84bb9
stigakl <stigakl@outlook.com>
2023-07-23 03:02:47 +0200

    ps profile

---
+++ Windows/profile.ps1
@@ -0,0 +1,75 @@
+
+$host.ui.RawUI.WindowTitle = "Hax0r"
+
+
+Import-Module posh-git
+
+$IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+#Invoke-Expression (&starship init powershell)
+
+
+Write-Host "Get-PID -port"
+function Get-PID {
+    param(
+        [string]$port
+    )
+    if($port -eq $null) {
+        return Write-Host "Missing port";
+    }
+    netstat -aon | findstr $port | ForEach-Object { ($_ -split "\s+")[-1] } | Get-Unique
+}
+
+Write-Host "Kill-Process -processId"
+function Kill-Process {
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        [string]$processId
+    )
+
+        if($processId -eq $null) {
+            return Write-Host "Missing process id"
+        }
+        taskkill /PID $processId /F
+        Write-Host "Killed process $processId"
+}
+function nopro {
+    #Assign Windows Title Text
+    $host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
+
+    #Configure current user, current folder and date outputs
+    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    $CmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+    $Date = Get-Date -Format 'dddd hh:mm:ss tt'
+
+    # Test for Admin / Elevated
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    #Calculate execution time of last cmd and convert to milliseconds, seconds or minutes
+    $LastCommand = Get-History -Count 1
+    if ($lastCommand) { $RunTime = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime).TotalSeconds }
+
+    if ($RunTime -ge 60) {
+        $ts = [timespan]::fromseconds($RunTime)
+        $min, $sec = ($ts.ToString("mm\:ss")).Split(":")
+        $ElapsedTime = -join ($min, " min ", $sec, " sec")
+    }
+    else {
+        $ElapsedTime = [math]::Round(($RunTime), 2)
+        $ElapsedTime = -join (($ElapsedTime.ToString()), " sec")
+    }
+
+    #Decorate the CMD Prompt
+    Write-Host ""
+    Write-host ($(if ($IsAdmin) { 'Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
+    Write-Host " USER:$($CmdPromptUser.Name.split("\")[1]) " -BackgroundColor DarkBlue -ForegroundColor White -NoNewline
+    If ($CmdPromptCurrentFolder -like "*:*")
+        {Write-Host " $CmdPromptCurrentFolder "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline}
+        else {Write-Host ".\$CmdPromptCurrentFolder\ "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline}
+
+    Write-Host " $date " -ForegroundColor White
+    Write-Host "[$elapsedTime] " -NoNewline -ForegroundColor Green
+    return "> "
+}
+
+. C:\Users\Stig\Documents\Programming\Powershell\tools\Cleanup-NodeModules.ps1
